0d91d8b9f953f106bc0fcf4b66cae88578ae9a6a
Anubhav Mishra <anubhavmishra271@gmail.com>
2020-07-30 23:56:00 +0530

    solved list.py

--- list-of-integers.py
+++ list-of-integers.py
@@ -1,6 +1,35 @@
-import matplotlib.pyplot as plt
-import numpy as np
+"""Write a function get_products_of_all_ints_except_at_index() that takes a list of integers and returns a list of the products.
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+For example, given:
+
+  [1, 7, 3, 4]
+
+Python 2.7
+your function would return:
+
+  [84, 12, 28, 21]
+
+Python 2.7
+by calculating:
+
+  [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3] """
+
+# CODE
+arr =   [1, 7, 3, 4]
+arr2 = []
+
+def get_products_of_all_ints_except_at_index(arr):
+    n=len(arr)
+    index=0
+    for j in range(0, n):
+        result =1
+        for i in range(0, n):
+            if i == index:
+                continue
+            else:
+                result = result* arr[i]
+        arr2.append(result)
+        index = index+1
+    print(arr2)
+
+get_products_of_all_ints_except_at_index(arr)
