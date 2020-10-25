64c0da4fa7325b1443748d26a3e0cf5d9e91f2f5
Anubhav Mishra <39110231+mishra-anubhav@users.noreply.github.com>
2020-10-25 15:47:51 +0530

    Update Network-marketing.py

--- Network-marketing.py
+++ Network-marketing.py
@@ -4,8 +4,12 @@ if ques=='Y':
     list = input()
     list = list.split(',')
     n = len(list)
-print("TOTAL MEMBERS :",n)
-print("COMMISSION DETAILS")
-print(parent,":",(n*500))
-for i in list:
-    print(i,": 250")
+    print("TOTAL MEMBERS :",n)
+    print("COMMISSION DETAILS")
+    print(parent,":",(n*500))
+    for i in list:
+        print(i,": 250")
+else:
+    print("TOTAL MEMBERS : 1")
+    print("COMMISSION DETAILS")
+    print(parent,":250")
